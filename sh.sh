e789eee6febf7cfb085e496828b87b294079cd1d
vpei <vpei@live.com>
2024-12-30 15:56:33 +0800

    Initial commit

--- sh.sh
+++ sh.sh
@@ -17,4 +17,11 @@
 # svn update
 # cd ../
 
+sed -i 's/24.12.18/24.12.28/g' feeds/packages/net/xray-core/Makefile
+sed -i 's/3d8b4a161a263e7af7bb1a2690961da075d13f980acd806f5cd4e5c8338d7534/9e7e14ac1da5aac22382b485bb2af3bbef07dc5e3b583e464c479f8f0f06b2d5/g' feeds/packages/net/xray-core/Makefile
+
+sed -i 's/1.9.7/1.10.5/g' feeds/packages/net/sing-box/Makefile
+sed -i 's/5b015352f3434bb780af01a6b1f6c0fe706166d6c44a69547e29892f0920b944/ca0385b45d160c2c2a1d0e09665f4f04caac27cb3dd9d6132173316dfd873b75/g' feeds/packages/net/sing-box/Makefile
+
 git pull && git add -A && git commit -am "Initial commit" && git push
+
