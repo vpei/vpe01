907de92e19cc8a8787f0ec5bd738ea92bd39a623
vpei <vpei@live.com>
2025-01-06 13:23:26 +0800

    Initial commit

--- sh.sh
+++ sh.sh
@@ -17,11 +17,11 @@
 # svn update
 # cd ../
 
-sed -i 's/24.12.18/24.12.28/g' feeds/packages/net/xray-core/Makefile
-sed -i 's/3d8b4a161a263e7af7bb1a2690961da075d13f980acd806f5cd4e5c8338d7534/9e7e14ac1da5aac22382b485bb2af3bbef07dc5e3b583e464c479f8f0f06b2d5/g' feeds/packages/net/xray-core/Makefile
+sed -i 's/24.12.28/25.1.1/g' feeds/packages/net/xray-core/Makefile
+sed -i 's/9e7e14ac1da5aac22382b485bb2af3bbef07dc5e3b583e464c479f8f0f06b2d5/619b10d24bab367a0788b3585f8d31fa4e5e060be7f72647d6ee17bd744603a5/g' feeds/packages/net/xray-core/Makefile
 
-sed -i 's/1.9.7/1.10.5/g' feeds/packages/net/sing-box/Makefile
-sed -i 's/5b015352f3434bb780af01a6b1f6c0fe706166d6c44a69547e29892f0920b944/ca0385b45d160c2c2a1d0e09665f4f04caac27cb3dd9d6132173316dfd873b75/g' feeds/packages/net/sing-box/Makefile
+sed -i 's/1.10.5/1.10.6/g' feeds/packages/net/sing-box/Makefile
+sed -i 's/ca0385b45d160c2c2a1d0e09665f4f04caac27cb3dd9d6132173316dfd873b75/bb3ca59c848f1509d0e183c54c91716993ce77ab0c9c48a0de0bcd4cc1f0e021/g' feeds/packages/net/sing-box/Makefile
 
 git pull && git add -A && git commit -am "Initial commit" && git push
 
