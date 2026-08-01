b2aaa303b4e4e16d3c2d9ab0de3e5f9bf68c9a5f
vpei <vpei@live.com>
2026-08-01 15:48:50 +0800

    Initial commit

--- luci-app-passwall/root/usr/share/passwall/helper_chinadns_add.lua
+++ luci-app-passwall/root/usr/share/passwall/helper_chinadns_add.lua
@@ -330,12 +330,16 @@ end
 --中国列表
 if CHNLIST ~= "0" and is_file_nonzero(RULES_PATH .. "/chnlist") then
 	if CHNLIST == "direct" then
+		local sets = {
+			setflag .. "psw_chn",
+			setflag .. "psw_chn6"
+		}
+		local suffix = (NFTFLAG == "1") and "_static" or ""
 		tmp_lines = {
 			"chnlist-file " .. RULES_PATH .. "/chnlist",
-			"ipset-name4 " .. setflag .. "psw_chn",
-			"ipset-name6 " .. setflag .. "psw_chn6",
-			"add-tagchn-ip",
-			"chnlist-first"
+			"ipset-name4 " .. setflag .. "psw_chn" .. suffix,
+			"ipset-name6 " .. setflag .. "psw_chn6" .. suffix,
+			"add-tagchn-ip" .. ((NFTFLAG == "1") and (" " .. table.concat(sets, ",")) or "")
 		}
 		merge_array(config_lines, tmp_lines)
 		log(string.format("  - 中国域名表(chnroute)：%s", DNS_LOCAL or "默认"))
